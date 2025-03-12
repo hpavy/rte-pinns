ec56e44a1a2f00ccad429c82decd22edfc4ca212
Hugo Pavy <hugo.pavy@gmail.com>
2025-03-12 10:00:25 +0100

    prediction ecoulement init

---
+++ prediction_ecoulement/main.py
@@ -0,0 +1,62 @@
+import torch
+from run import RunSimulation
+from constants import DICT_Y0, DICT_CASE, PARAM_ADIM, M
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Le code se lance sur {device}")
+
+
+folder_result_name = "1_reconstruction_avec_pinns"  # Le nom du fichier de résultats
+
+# On utilise hyperparam_init uniquement si c'est un nouveau modèle
+hyperparam_init = {
+    "num": [1],
+    "case": [2],
+    "nb_epoch": 1000,  # le nombre d'epoch
+    "save_rate": 10,
+    "with_pinns": False,  # Si on utilise la loss
+    "dynamic_weights": False,  # Est ce qu'on fait bouger de manière dynamique les poids
+    "lr_weights": 0.1,  # la learning rate pour les poids si on les fait bouger 
+    "weight_data": 0.33,  # le poids initial de la data
+    "weight_pde": 0.33,
+    "weight_border": 0.33,
+    "batch_size": 10000,  # La taille d'un batch du pde
+    "nb_points_pde": 1000000,  # Le nombre total de points pour le pde
+    "Re": 100,
+    "lr_init": 5e-4,
+    "gamma_scheduler": 0.999,  # Pour la learning rate
+    "nb_layers": 10,
+    "nb_neurons": 64,
+    "n_pde_test": 500,
+    "n_data_test": 5000,
+    "nb_points": 80,  # Nombre de points sur chaque pas de temps 
+    # Les limites sur lesquelles on entraîne
+    "x_min": -0.1,     
+    "x_max": 0.1,
+    "y_min": -0.1,
+    "y_max": 0.1,
+    "t_min": 6.5,
+    "nb_period": 5,  # le nombre de période qu'on prend (on ramène tout sur une seule pour avoir plus de pas de temps)
+    # "nb_period_plot": 2,
+    "force_inertie_bool": False,  # Est ce qu'on prend la force d'inertie
+    # Les conditons au bord
+    "u_border": False,
+    "v_border": False,
+    "p_border": False,
+    "r_min": 0.026/2,  # Le rayon minimal à partir du quel on prend les données
+    'theta_border_min': 0.1,
+    'is_res': True,  # Est ce qu'on utilise un res net
+    'nb_blocks': 10,  # Pour ResNet
+    'nb_layer_block': 3  # Pour ResNet
+}
+
+hyperparam_init['H'] = [DICT_CASE[str(k)] for k in hyperparam_init['case']]
+hyperparam_init['ya0'] = [DICT_Y0[str(k)] for k in hyperparam_init['num']]
+hyperparam_init['file'] = [
+    f"model_{num_}_case_{case_}.csv" for num_, case_ in zip(hyperparam_init['num'], hyperparam_init['case'])
+    ]
+hyperparam_init['m'] = M
+
+simu = RunSimulation(hyperparam_init, folder_result_name, PARAM_ADIM)  
+
+simu.run()  # début de la simulation
---
+++ prediction_ecoulement/run.py
@@ -0,0 +1,116 @@
+from geometry import RectangleWithoutCylinder
+import torch
+from utils import charge_data, init_model
+from train import train
+from pathlib import Path
+import time
+import pandas as pd
+import numpy as np
+import json
+
+
+class RunSimulation:
+    def __init__(self, hyperparam, folder_result_name, param_adim):
+        self.hyperparam = hyperparam
+        self.time_start = time.time()
+        self.folder_result_name = folder_result_name
+        self.folder_result = "results/" + folder_result_name
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.param_adim = param_adim
+        self.nb_simu = len(self.hyperparam["file"])
+
+    def run(self):
+        # Creation du dossier de result
+        Path(self.folder_result).mkdir(parents=True, exist_ok=True)
+        if not Path(self.folder_result + "/hyperparam.json").exists():
+            with open(self.folder_result + "/hyperparam.json", "w") as file:
+                json.dump(self.hyperparam, file, indent=4)
+            self.hyperparam = self.hyperparam
+
+        else:
+            with open(self.folder_result + "/hyperparam.json", "r") as file:
+                self.hyperparam = json.load(file)
+
+        # Chargement des données
+        (
+            X_train,
+            U_train,
+            X_full,
+            U_full,
+            X_border_train,
+            X_border_test,
+            U_border_train, 
+            U_border_test,
+            mean_std,
+            X_pde,
+            X_test_pde,
+            X_test_data,
+            U_test_data,
+        ) = charge_data(self.hyperparam, self.param_adim)
+        X_train.requires_grad_()
+        U_train.requires_grad_()
+        X_border_train.requires_grad_()
+        U_border_train.requires_grad_()
+
+        # On écrit mean_std
+        mean_std_json = dict()
+        for key in mean_std:
+            mean_std_json[key] = mean_std[key].item()
+        with open(self.folder_result + "/mean_std.json", "w") as file:
+            json.dump(mean_std_json, file, indent=4)
+
+        # On plot les print dans un fichier texte
+        with open(self.folder_result + "/print.txt", "a") as f:
+            model, optimizer, scheduler, loss, train_loss, test_loss, weights = (
+                init_model(f, self.hyperparam, self.device, self.folder_result)
+            )
+            
+            if self.hyperparam["dynamic_weights"]:
+                weight_data = weights["weight_data"]
+                weight_pde = weights["weight_pde"]
+                weight_border = weights["weight_border"]
+            else:
+                weight_data = self.hyperparam["weight_data"]
+                weight_pde = self.hyperparam["weight_pde"]
+                weight_border = self.hyperparam["weight_border"]
+                # Data loading
+
+            # On entraine le modèle
+            train(
+                nb_epoch=1000,
+                train_loss=train_loss,
+                test_loss=test_loss,
+                weight_data_init=weight_data,
+                weight_pde_init=weight_pde,
+                weight_border_init=weight_border,
+                dynamic_weights=self.hyperparam["dynamic_weights"],
+                lr_weights=self.hyperparam["lr_weights"],
+                model=model,
+                loss=loss,
+                optimizer=optimizer,
+                X_train=X_train,
+                U_train=U_train,
+                X_pde=X_pde,
+                X_test_pde=X_test_pde,
+                X_test_data=X_test_data,
+                U_test_data=U_test_data,
+                Re=self.hyperparam["Re"],
+                time_start=self.time_start,
+                f=f,
+                folder_result=self.folder_result,
+                save_rate=self.hyperparam["save_rate"],
+                batch_size=self.hyperparam["batch_size"],
+                scheduler=scheduler,
+                X_border_train=X_border_train,
+                X_border_test=X_border_test,
+                U_border_train=U_border_train,
+                U_border_test=U_border_test,
+                mean_std=mean_std,
+                param_adim=self.param_adim,
+                nb_simu=len(self.hyperparam["file"]),
+                force_inertie_bool=self.hyperparam["force_inertie_bool"],
+                u_border=self.hyperparam['u_border'],
+                v_border=self.hyperparam['v_border'],
+                p_border=self.hyperparam['p_border'],
+                with_pinns=self.hyperparam['with_pinns']
+            )
